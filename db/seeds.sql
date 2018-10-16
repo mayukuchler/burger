9b3c97fb3e5dbcb7f1b9902be61aa89e60e90222
Lucy <mayukuchler@gmail.com>
2018-10-15 21:33:17 -0400

    add

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Perfecto Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Hamburger', true);
